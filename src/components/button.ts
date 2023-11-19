50b86ee0cb89ed6317a0cc3a9d920a7e41391934
jp calvo <calvojp92@gmail.com>
2023-11-19 21:39:55 +0800

    add default props

--- src/components/button.ts
+++ src/components/button.ts
@@ -2,4 +2,8 @@ import {styled} from '@/styled-system/jsx';
 import {button} from '@/styled-system/recipes';
 import {ark} from '@ark-ui/react';
 
-export const Button = styled(ark.button, button);
+export const Button = styled(ark.button, button, {
+	defaultProps: {
+		type: 'button',
+	},
+});
