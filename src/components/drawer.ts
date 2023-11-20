efac1088d1b305577332e8d2de2d83b6f5927ebe
jp calvo <calvojp92@gmail.com>
2023-11-20 20:45:37 +0800

    add drawer

---
+++ src/components/drawer.ts
@@ -0,0 +1,20 @@
+'use client';
+
+import {styled} from '@/styled-system/jsx';
+import {drawer} from '@/styled-system/recipes';
+import {createStyleContext} from '@/utils/create-style-context';
+import {ark, Dialog as parts} from '@ark-ui/react';
+
+const {withProvider, withContext} = createStyleContext(drawer);
+
+export const Drawer = withProvider(styled(parts.Root));
+export const DrawerBackdrop = withContext(styled(parts.Backdrop), 'backdrop');
+export const DrawerCloseTrigger = withContext(styled(parts.CloseTrigger), 'closeTrigger');
+export const DrawerPositioner = withContext(styled(parts.Positioner), 'positioner');
+export const DrawerContent = withContext(styled(parts.Content), 'content');
+export const DrawerDescription = withContext(styled(parts.Description), 'description');
+export const DrawerTitle = withContext(styled(parts.Title), 'title');
+export const DrawerTrigger = withContext(styled(parts.Trigger), 'trigger');
+export const DrawerHeader = withContext(styled(ark.div), 'header');
+export const DrawerBody = withContext(styled(ark.div), 'body');
+export const DrawerFooter = withContext(styled(ark.div), 'footer');
