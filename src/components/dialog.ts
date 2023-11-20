59ccc70886117c17e0c8f3b4f115674bd8d3fd61
jp calvo <calvojp92@gmail.com>
2023-11-20 20:50:37 +0800

    add dialog

---
+++ src/components/dialog.ts
@@ -0,0 +1,17 @@
+'use client';
+
+import {styled} from '@/styled-system/jsx';
+import {dialog} from '@/styled-system/recipes';
+import {createStyleContext} from '@/utils/create-style-context';
+import {Dialog as parts} from '@ark-ui/react';
+
+const {withProvider, withContext} = createStyleContext(dialog);
+
+export const Dialog = withProvider(styled(parts.Root));
+export const DialogBackdrop = withContext(styled(parts.Backdrop), 'backdrop');
+export const DialogCloseTrigger = withContext(styled(parts.CloseTrigger), 'closeTrigger');
+export const DialogContent = withContext(styled(parts.Content), 'content');
+export const DialogDescription = withContext(styled(parts.Description), 'description');
+export const DialogPositioner = withContext(styled(parts.Positioner), 'positioner');
+export const DialogTitle = withContext(styled(parts.Title), 'title');
+export const DialogTrigger = withContext(styled(parts.Trigger), 'trigger');
