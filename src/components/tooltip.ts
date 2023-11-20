a4a2bfadbae75f4d97fea1d606da1c46b65abbe0
jp calvo <calvojp92@gmail.com>
2023-11-20 20:10:33 +0800

    add tooltip

---
+++ src/components/tooltip.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import {styled} from '@/styled-system/jsx';
+import {tooltip} from '@/styled-system/recipes';
+import {createStyleContext} from '@/utils/create-style-context';
+import {Tooltip as parts} from '@ark-ui/react';
+
+const {withProvider, withContext} = createStyleContext(tooltip);
+
+export const Tooltip = withProvider(styled(parts.Root));
+export const TooltipArrow = withContext(styled(parts.Arrow), 'arrow');
+export const TooltipArrowTip = withContext(styled(parts.ArrowTip), 'arrowTip');
+export const TooltipContent = withContext(styled(parts.Content), 'content');
+export const TooltipPositioner = withContext(styled(parts.Positioner), 'positioner');
+export const TooltipTrigger = withContext(styled(parts.Trigger), 'trigger');
