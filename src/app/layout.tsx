ed21671eff9a1a6f0bdf6ca29b1f1542b34fe61d
jp calvo <calvojp92@gmail.com>
2023-11-20 22:32:13 +0800

    rename project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import './globals.css';
 const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
-	title: 'Budget Tracker',
+	title: 'Expense Tracker',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
