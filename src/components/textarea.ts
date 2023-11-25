ba493b262cd6fea5b946847599d402011c614146
jp calvo <calvojp92@gmail.com>
2023-11-25 14:04:31 +0800

    rm textarea

--- src/components/textarea.ts
+++
@@ -1,5 +0,0 @@
-import {styled} from '@/styled-system/jsx';
-import {textarea} from '@/styled-system/recipes';
-import {ark} from '@ark-ui/react';
-
-export const Textarea = styled(ark.textarea, textarea);
