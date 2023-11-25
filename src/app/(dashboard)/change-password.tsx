6b5ca881be07ff2dea67d050b29f7dcafcf035c6
jp calvo <calvojp92@gmail.com>
2023-11-25 18:52:38 +0800

    init change password

--- src/app/(dashboard)/change-password.tsx
+++ src/app/(dashboard)/change-password.tsx
@@ -1,19 +1,45 @@
 'use client';
 
+import {
+	Dialog,
+	DialogBackdrop,
+	DialogContent,
+	DialogPositioner,
+	DialogTrigger,
+} from '@/components/dialog';
 import {Icon} from '@/components/icon';
 import {MenuItem} from '@/components/menu';
-import {HStack, styled} from '@/styled-system/jsx';
+import {Box, HStack, styled} from '@/styled-system/jsx';
+import {Portal} from '@ark-ui/react';
 import {LockIcon} from 'lucide-react';
 
 export function ChangePassword() {
 	return (
-		<MenuItem id="navbar.profile-settings.change-password">
-			<HStack>
-				<Icon>
-					<LockIcon />
-				</Icon>
-				<styled.span>Change Password</styled.span>
-			</HStack>
-		</MenuItem>
+		<Dialog
+			lazyMount
+			unmountOnExit
+			closeOnEscapeKeyDown={false}
+			closeOnInteractOutside={false}
+		>
+			<DialogTrigger asChild>
+				<MenuItem id="navbar.profile-settings.change-password">
+					<HStack>
+						<Icon>
+							<LockIcon />
+						</Icon>
+						<styled.span>Change Password</styled.span>
+					</HStack>
+				</MenuItem>
+			</DialogTrigger>
+
+			<Portal>
+				<DialogBackdrop />
+				<DialogPositioner>
+					<DialogContent asChild>
+						<Box></Box>
+					</DialogContent>
+				</DialogPositioner>
+			</Portal>
+		</Dialog>
 	);
 }
