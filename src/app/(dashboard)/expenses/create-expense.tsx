1b8168b1312f87084e8384b778d18fb368300720
jp calvo <calvojp92@gmail.com>
2023-11-25 14:35:49 +0800

    refactor

--- src/app/(dashboard)/expenses/create-expense.tsx
+++ src/app/(dashboard)/expenses/create-expense.tsx
@@ -9,56 +9,44 @@ import {
 	DialogDescription,
 	DialogPositioner,
 	DialogTitle,
+	DialogTrigger,
 } from '@/components/dialog';
 import {Icon} from '@/components/icon';
 import {IconButton} from '@/components/icon-button';
 import {Portal} from '@ark-ui/react';
 import {PlusIcon, XIcon} from 'lucide-react';
-import {useState} from 'react';
 
 export function CreateExpense() {
-	const [open, setOpen] = useState(false);
-
 	return (
-		<>
-			<Button variant="outline" onClick={() => setOpen(true)}>
-				<Icon>
-					<PlusIcon />
-				</Icon>
-				Add new
-			</Button>
-
-			<Dialog
-				open={open}
-				onOpenChange={(details) => {
-					setOpen(details.open);
-				}}
-				lazyMount
-				unmountOnExit
-				closeOnEscapeKeyDown={false}
-				closeOnInteractOutside={false}
-			>
-				<Portal>
-					<DialogBackdrop />
-					<DialogPositioner>
-						<DialogContent>
-							<DialogTitle>Dialog Title</DialogTitle>
-							<DialogDescription>Dialog Description</DialogDescription>
-							<DialogCloseTrigger
-								asChild
-								position="absolute"
-								top="2"
-								right="2"
-								onClick={() => {}}
-							>
-								<IconButton aria-label="Close Dialog" variant="ghost" size="sm">
-									<XIcon />
-								</IconButton>
-							</DialogCloseTrigger>
-						</DialogContent>
-					</DialogPositioner>
-				</Portal>
-			</Dialog>
-		</>
+		<Dialog lazyMount unmountOnExit>
+			<DialogTrigger asChild>
+				<Button variant="outline">
+					<Icon>
+						<PlusIcon />
+					</Icon>
+					Add new
+				</Button>
+			</DialogTrigger>
+			<Portal>
+				<DialogBackdrop />
+				<DialogPositioner>
+					<DialogContent>
+						<DialogTitle>Dialog Title</DialogTitle>
+						<DialogDescription>Dialog Description</DialogDescription>
+						<DialogCloseTrigger
+							asChild
+							position="absolute"
+							top="2"
+							right="2"
+							onClick={() => {}}
+						>
+							<IconButton aria-label="Close Dialog" variant="ghost" size="sm">
+								<XIcon />
+							</IconButton>
+						</DialogCloseTrigger>
+					</DialogContent>
+				</DialogPositioner>
+			</Portal>
+		</Dialog>
 	);
 }
--- src/app/(dashboard)/expenses/delete-expense.tsx
+++ src/app/(dashboard)/expenses/delete-expense.tsx
@@ -8,6 +8,7 @@ import {
 	DialogDescription,
 	DialogPositioner,
 	DialogTitle,
+	DialogTrigger,
 } from '@/components/dialog';
 import {Icon} from '@/components/icon';
 import {IconButton} from '@/components/icon-button';
@@ -16,65 +17,50 @@ import {toast} from '@/components/toaster';
 import {HStack, styled} from '@/styled-system/jsx';
 import {Portal} from '@ark-ui/react';
 import {FileX2Icon, XIcon} from 'lucide-react';
-import {useId, useState} from 'react';
+import {useId} from 'react';
 
 export function DeleteExpense() {
 	const id = useId();
-	const [open, setOpen] = useState(false);
 
 	return (
-		<>
-			<MenuItem
-				id={`expenses.items.${id}.menu.delete`}
-				onClick={() => {
-					setOpen(true);
-				}}
-			>
-				<HStack>
-					<Icon>
-						<FileX2Icon />
-					</Icon>
-					<styled.span>Delete</styled.span>
-				</HStack>
-			</MenuItem>
+		<Dialog lazyMount unmountOnExit>
+			<DialogTrigger asChild>
+				<MenuItem id={`expenses.items.${id}.menu.delete`}>
+					<HStack>
+						<Icon>
+							<FileX2Icon />
+						</Icon>
+						<styled.span>Delete</styled.span>
+					</HStack>
+				</MenuItem>
+			</DialogTrigger>
 
-			<Dialog
-				open={open}
-				onOpenChange={(details) => {
-					setOpen(details.open);
-				}}
-				lazyMount
-				unmountOnExit
-				closeOnEscapeKeyDown={false}
-				closeOnInteractOutside={false}
-			>
-				<Portal>
-					<DialogBackdrop />
-					<DialogPositioner>
-						<DialogContent>
-							<DialogTitle>Dialog Title</DialogTitle>
-							<DialogDescription>Dialog Description</DialogDescription>
+			<Portal>
+				<DialogBackdrop />
+				<DialogPositioner>
+					<DialogContent>
+						<DialogTitle>Dialog Title</DialogTitle>
+						<DialogDescription>Dialog Description</DialogDescription>
 
-							<DialogCloseTrigger
-								asChild
-								position="absolute"
-								top="2"
-								right="2"
-								onClick={() => {
-									toast.success({
-										title: 'Success',
-										description: 'Item has been deleted',
-									});
-								}}
-							>
-								<IconButton aria-label="Close Dialog" variant="ghost" size="sm">
-									<XIcon />
-								</IconButton>
-							</DialogCloseTrigger>
-						</DialogContent>
-					</DialogPositioner>
-				</Portal>
-			</Dialog>
-		</>
+						<DialogCloseTrigger
+							asChild
+							position="absolute"
+							top="2"
+							right="2"
+							onClick={() => {
+								toast.success({
+									title: 'Success',
+									description: 'Item has been deleted',
+								});
+							}}
+						>
+							<IconButton aria-label="Close Dialog" variant="ghost" size="sm">
+								<XIcon />
+							</IconButton>
+						</DialogCloseTrigger>
+					</DialogContent>
+				</DialogPositioner>
+			</Portal>
+		</Dialog>
 	);
 }
