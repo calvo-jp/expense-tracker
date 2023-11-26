70f292f00b7be1cea7a5f396ab397cf3e74a98c0
jp calvo <calvojp92@gmail.com>
2023-11-26 14:32:04 +0800

    refactor

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -1,61 +1,54 @@
 'use server';
 
 import {prisma} from '@/config/prisma';
-import {formdataToJson} from '@/utils/formdata-to-json';
 import bcrypt from 'bcrypt';
 import {addDays} from 'date-fns';
 import {cookies} from 'next/headers';
-import {redirect} from 'next/navigation';
-import {z} from 'zod';
+import {CredentialsSchema} from './schema';
 
-const CredentialsSchema = z.object({
-	username: z
-		.string()
-		.min(5, 'Username too short')
-		.max(25, 'Username too long')
-		.regex(/^[a-z][a-z0-9]{4,}$/i, 'Invalid username')
-		.trim(),
-	password: z
-		.string()
-		.min(8, 'Password too short')
-		.max(150, 'Password too long'),
-});
+export async function login(input: unknown) {
+	const parsed = CredentialsSchema.safeParse(input);
 
-export async function login(_: unknown, formdata: FormData) {
-	const cookieStore = cookies();
+	if (!parsed.success) return parsed.error.errors[0].message;
+
+	const {username, password} = parsed.data;
 
 	try {
-		const parsed = CredentialsSchema.safeParse(formdataToJson(formdata));
-
-		if (!parsed.success) return parsed.error.errors[0].message;
-
-		const {username, password} = parsed.data;
-
 		const user = await prisma.user.findUniqueOrThrow({where: {username}});
 		const matches = await bcrypt.compare(password, user.password);
 
 		if (!matches) throw new Error();
 
-		cookieStore.set('user', user.id, {expires: addDays(new Date(), 30)});
-		redirect('/dashboard');
+		cookies().set('user', user.id, {expires: addDays(new Date(), 30)});
+		return null;
 	} catch {
 		return 'Invalid username or password';
 	}
 }
 
-export async function register(_: unknown, formdata: FormData) {
-	const cookieStore = cookies();
+export async function register(input: unknown) {
+	const parsed = CredentialsSchema.safeParse(input);
+
+	if (!parsed.success) return parsed.error.errors[0].message;
+
+	let {username, password} = parsed.data;
+
+	password = await bcrypt.hash(password, await bcrypt.genSalt(16));
 
 	try {
-		const data = CredentialsSchema.parse(formdataToJson(formdata));
+		const user = await prisma.user.create({
+			data: {
+				username,
+				password,
+			},
+			select: {
+				id: true,
+			},
+		});
 
-		data.password = await bcrypt.hash(data.password, await bcrypt.genSalt(16));
-
-		const user = await prisma.user.create({data});
-
-		cookieStore.set('user', user.id, {expires: addDays(new Date(), 30)});
-		redirect('/dashboard');
-	} catch (error) {
+		cookies().set('user', user.id, {expires: addDays(new Date(), 30)});
+		return null;
+	} catch {
 		return 'Invalid username or password';
 	}
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,16 +1,60 @@
-import {Link} from '@/components/next-js/link';
-import {Flex, styled} from '@/styled-system/jsx';
-import {Metadata} from 'next';
-import {RegisterForm} from './register-form';
+'use client';
 
-export const metadata: Metadata = {
-	title: 'Register',
-};
+import {Button} from '@/components/button';
+import {Input} from '@/components/input';
+import {Link} from '@/components/next-js/link';
+import {toast} from '@/components/toaster';
+import {Flex, styled} from '@/styled-system/jsx';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useRouter} from 'next/navigation';
+import {useTransition} from 'react';
+import {useForm} from 'react-hook-form';
+import {register} from '../actions';
+import {CredentialsSchema} from '../schema';
 
 export default function Register() {
+	const [pending, startTransition] = useTransition();
+
+	const router = useRouter();
+	const form = useForm({
+		resolver: zodResolver(CredentialsSchema),
+	});
+
 	return (
 		<>
-			<RegisterForm />
+			<styled.form
+				onSubmit={form.handleSubmit((data) => {
+					return startTransition(async () => {
+						const error = await register(data);
+
+						if (error) {
+							toast.error({
+								title: 'Error',
+								description: error,
+							});
+						} else {
+							router.push('/dashboard');
+						}
+					});
+				})}
+			>
+				<Input
+					size="xl"
+					placeholder="Username"
+					{...form.register('username')}
+				/>
+				<Input
+					mt={6}
+					size="xl"
+					type="password"
+					placeholder="Password"
+					{...form.register('password')}
+				/>
+
+				<Button type="submit" w="full" mt={8} size="xl" disabled={pending}>
+					Submit
+				</Button>
+			</styled.form>
 
 			<Flex mt={5} gap={1} justifyContent="center">
 				<styled.span color="fg.muted">Already have an account?</styled.span>
