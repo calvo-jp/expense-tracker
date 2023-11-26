1dcb6b9be1177ca0071d47c91a0ac3a73a1c7ddb
jp calvo <calvojp92@gmail.com>
2023-11-26 20:32:00 +0800

    refactor

--- src/app/(dashboard)/reports/export.tsx
+++ src/app/(dashboard)/reports/export.tsx
@@ -1,13 +1,26 @@
+'use client';
+
 import {Icon} from '@/components/icon';
 import {IconButton} from '@/components/icon-button';
 import {FileArchiveIcon} from 'lucide-react';
+import {useTransition} from 'react';
 
 export function Export() {
+	const [pending, startTransition] = useTransition();
+
 	return (
-		<IconButton type="submit" variant="outline">
+		<IconButton
+			variant="outline"
+			disabled={pending}
+			onClick={() => startTransition(download)}
+		>
 			<Icon>
 				<FileArchiveIcon />
 			</Icon>
 		</IconButton>
 	);
+}
+
+async function download() {
+	/* logic */
 }
