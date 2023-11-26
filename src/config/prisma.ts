090efcd4c6f5fe77ec49c7d6fe98f8a38242458b
jp calvo <calvojp92@gmail.com>
2023-11-26 12:42:45 +0800

    refactor

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,11 +1,8 @@
 import {PrismaClient} from '@prisma/client';
 
 const g = globalThis as unknown as {prisma: PrismaClient};
+const i = g.prisma ?? new PrismaClient();
 
-export const prisma = g.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV === 'development') g.prisma = i;
 
-if (process.env.NODE_ENV === 'production') {
-	/* skip */
-} else {
-	g.prisma = prisma;
-}
+export const prisma = i;
