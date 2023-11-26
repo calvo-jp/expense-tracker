eb2249b9577ca8af4e55f2f23e31d12809f74338
jp calvo <calvojp92@gmail.com>
2023-11-26 15:01:04 +0800

    refactor

--- src/app/(dashboard)/reports/export.tsx
+++ src/app/(dashboard)/reports/export.tsx
@@ -1,22 +1,13 @@
 import {Icon} from '@/components/icon';
 import {IconButton} from '@/components/icon-button';
-import {styled} from '@/styled-system/jsx';
 import {FileArchiveIcon} from 'lucide-react';
 
 export function Export() {
 	return (
-		<styled.form
-			action={async () => {
-				'use server';
-
-				/* logic */
-			}}
-		>
-			<IconButton type="submit" variant="outline">
-				<Icon>
-					<FileArchiveIcon />
-				</Icon>
-			</IconButton>
-		</styled.form>
+		<IconButton type="submit" variant="outline">
+			<Icon>
+				<FileArchiveIcon />
+			</Icon>
+		</IconButton>
 	);
 }
