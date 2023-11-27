1d8fbefb3baea4353c35ddbe55316e6c9d9431af
jp calvo <calvojp92@gmail.com>
2023-11-27 19:07:31 +0800

    refactor error message

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -3,23 +3,25 @@ import {Assign, HTMLStyledProps} from "@/styled-system/types";
 import {HTMLArkProps, ark} from "@ark-ui/react";
 import {forwardRef} from "react";
 
-const StyledArkDiv = styled(ark.div);
-
-type ErrorMessageProps = Assign<HTMLArkProps<"div">, HTMLStyledProps<"div">>;
-
-export const ErrorMessage = forwardRef<HTMLDivElement, ErrorMessageProps>(
-	function ErrorMessage(props, ref) {
-		if (!props.children) return null;
-
-		return (
-			<StyledArkDiv
-				ref={ref}
-				color="red.a11"
-				fontSize="sm"
-				lineHeight="none"
-				role="alert"
-				{...props}
-			/>
-		);
+const Component = styled(
+	ark.div,
+	{
+		base: {
+			color: "red.a11",
+			fontSize: "sm",
+			lineHeight: "none",
+		},
+	},
+	{
+		defaultProps: {
+			role: "alert",
+		},
 	},
 );
+
+export const ErrorMessage = forwardRef<
+	HTMLDivElement,
+	Assign<HTMLArkProps<"div">, HTMLStyledProps<"div">>
+>(function ErrorMessage(props, ref) {
+	return props.children ? <Component ref={ref} {...props} /> : null;
+});
