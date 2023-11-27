3e7ac8f522e4221c8d53745222157ab98458b5dc
jp calvo <calvojp92@gmail.com>
2023-11-27 19:17:34 +0800

    move navbar

---
+++ src/app/(landing)/navbar.tsx
@@ -0,0 +1,28 @@
+import {Button} from "@/components/button";
+import {Link} from "@/components/next-js/link";
+import {HStack, Spacer, styled} from "@/styled-system/jsx";
+import {Logo} from "../logo";
+
+export function Navbar() {
+	return (
+		<styled.header
+			h="navbar.height"
+			px={8}
+			py={3}
+			display="flex"
+			alignItems="center"
+			borderBottomWidth="1px"
+		>
+			<Logo />
+			<Spacer />
+			<HStack gap={3}>
+				<Button asChild variant="ghost">
+					<Link href="/login">Login</Link>
+				</Button>
+				<Button asChild variant="outline">
+					<Link href="/register">Register</Link>
+				</Button>
+			</HStack>
+		</styled.header>
+	);
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import {styled} from "@/styled-system/jsx";
+import {Navbar} from "./navbar";
+
+export default async function Index() {
+	return (
+		<>
+			<Navbar />
+			<styled.main></styled.main>
+		</>
+	);
+}
