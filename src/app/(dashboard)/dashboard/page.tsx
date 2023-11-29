6537e1970eedb3c71b3fd720dd5490d409cc5ef4
jp calvo <calvojp92@gmail.com>
2023-11-29 18:49:12 +0800

    test

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import {prisma} from "@/config/prisma";
 import {Box, Flex, Spacer, styled} from "@/styled-system/jsx";
+import {WebServiceClient} from "@maxmind/geoip2-node";
 import {Metadata} from "next";
-import {cookies} from "next/headers";
+import {cookies, headers} from "next/headers";
+import {Suspense} from "react";
 
 export const metadata: Metadata = {
 	title: "Dashboard",
@@ -39,7 +41,31 @@ export default async function Dashboard() {
 					</styled.p>
 				</Box>
 				<Spacer />
+				<Suspense fallback={null}>
+					<YearSummary />
+				</Suspense>
+				<Suspense fallback={null}>
+					<MonthSummary />
+				</Suspense>
 			</Flex>
 		</Box>
 	);
+}
+
+async function YearSummary() {
+	const client = new WebServiceClient(
+		process.env.MAXMIND_ACCOUNT_ID!,
+		process.env.MAXMIND_LICENSE_KEY!,
+		{
+			host: "geolite.info",
+		},
+	);
+
+	const {city} = await client.city(headers().get("x-forwarded-for")!);
+
+	return <div>{city?.names.en}</div>;
+}
+
+async function MonthSummary() {
+	return null;
 }
