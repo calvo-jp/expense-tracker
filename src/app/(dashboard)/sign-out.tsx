4c5bd3734c499c6606084b2da5b39ae45f4eeac2
jp calvo <calvojp92@gmail.com>
2023-11-30 16:28:53 +0800

    refactor

--- src/app/(dashboard)/sign-out.tsx
+++ src/app/(dashboard)/sign-out.tsx
@@ -7,25 +7,27 @@ import {PowerIcon} from "lucide-react";
 import {useRouter} from "next/navigation";
 import {forwardRef} from "react";
 
-export const Signout = forwardRef<HTMLButtonElement, HTMLStyledProps<"button">>(
-	function Signout(props, ref) {
-		const router = useRouter();
+export const Signout = forwardRef<
+	HTMLButtonElement,
+	Omit<HTMLStyledProps<"button">, "children">
+>(function Signout({onClick, ...props}, ref) {
+	const router = useRouter();
 
-		return (
-			<styled.button
-				ref={ref}
-				{...props}
-				onClick={() => {
-					logout().then(() => {
-						router.push("/login");
-					});
-				}}
-			>
-				<Icon>
-					<PowerIcon />
-				</Icon>
-				<styled.span>Sign out</styled.span>
-			</styled.button>
-		);
-	},
-);
+	return (
+		<styled.button
+			ref={ref}
+			onClick={(e) => {
+				onClick?.(e);
+				logout().then(() => {
+					router.push("/login");
+				});
+			}}
+			{...props}
+		>
+			<Icon>
+				<PowerIcon />
+			</Icon>
+			<styled.span>Sign out</styled.span>
+		</styled.button>
+	);
+});
