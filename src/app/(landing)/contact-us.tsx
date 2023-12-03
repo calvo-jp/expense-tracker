8e0fafc59461fa2db20dfcb00b1d6488c979a143
jp calvo <calvojp92@gmail.com>
2023-12-03 20:26:10 +0800

    add contact us

--- src/app/(landing)/contact-us.tsx
+++ src/app/(landing)/contact-us.tsx
@@ -1,3 +1,35 @@
+import {Button} from "@/components/button";
+import {Input} from "@/components/input";
+import {Textarea} from "@/components/textarea";
+import {Box, styled} from "@/styled-system/jsx";
+
 export function ContactUs() {
-	return null;
+	return (
+		<Box maxW="breakpoint-md" mx="auto" py={24} px={8}>
+			<styled.h2
+				textAlign="center"
+				fontFamily="heading"
+				fontWeight="bold"
+				fontSize="4xl"
+			>
+				Get in Touch
+			</styled.h2>
+
+			<styled.form
+				mt={12}
+				maxW="26rem"
+				mx="auto"
+				display="flex"
+				flexDir="column"
+				gap={5}
+			>
+				<Input size="xl" placeholder="Name" />
+				<Input size="xl" placeholder="Email" />
+				<Textarea size="xl" placeholder="Message" />
+				<Button size="xl" display="block">
+					Submit
+				</Button>
+			</styled.form>
+		</Box>
+	);
 }
