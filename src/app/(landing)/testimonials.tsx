e287dc2dd02ffb071a1a46074688c4e42fbfd202
jp calvo <calvojp92@gmail.com>
2023-12-03 21:14:53 +0800

    add testimonials

--- src/app/(landing)/testimonials.tsx
+++ src/app/(landing)/testimonials.tsx
@@ -1,5 +1,65 @@
-import {Box} from "@/styled-system/jsx";
+import {Avatar, AvatarImage} from "@/components/avatar";
+import {Icon} from "@/components/icon";
+import {Box, Flex, Grid, GridItem, HStack, styled} from "@/styled-system/jsx";
+import {QuoteIcon} from "lucide-react";
 
 export function Testimonials() {
-	return <Box></Box>;
+	return (
+		<Box maxW="breakpoint-lg" mx="auto" py={24} px={8}>
+			<styled.h2
+				textAlign="center"
+				fontFamily="heading"
+				fontWeight="bold"
+				fontSize="4xl"
+			>
+				Testimonials
+			</styled.h2>
+
+			<Grid mt={12} columns={2} gap={6}>
+				<GridItem>
+					<Testimony />
+				</GridItem>
+				<GridItem>
+					<Testimony />
+				</GridItem>
+			</Grid>
+		</Box>
+	);
+}
+
+function Testimony() {
+	return (
+		<Flex
+			p={8}
+			borderWidth="1px"
+			rounded="sm"
+			flexDir="column"
+			gap={6}
+			bg="bg.subtle"
+		>
+			<Icon size="xl" color="fg.disabled">
+				<QuoteIcon />
+			</Icon>
+
+			<styled.p flexGrow={1}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dicta
+				nostrum unde natus ducimus iste itaque reiciendis repellat perspiciatis
+				beatae?
+			</styled.p>
+
+			<HStack>
+				<Avatar>
+					<AvatarImage src="https://i.pravatar.cc/300" />
+				</Avatar>
+				<Box>
+					<Box fontSize="sm" fontWeight="medium">
+						Mark Zuckerberg
+					</Box>
+					<Box fontSize="xs" color="fg.muted">
+						CEO at Facebook
+					</Box>
+				</Box>
+			</HStack>
+		</Flex>
+	);
 }
