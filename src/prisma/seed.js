9801c411d260fdb92abb7d3eca22e8cdcb858de0
jp calvo <calvojp92@gmail.com>
2023-12-05 19:04:40 +0800

    add seed

---
+++ src/prisma/seed.js
@@ -0,0 +1,48 @@
+// @ts-check
+
+const {PrismaClient, ExpenseCategory} = require("@prisma/client");
+const {subYears} = require("date-fns");
+const {faker} = require("@faker-js/faker");
+const bcrypt = require("bcrypt");
+const prisma = new PrismaClient();
+
+async function main() {
+	await prisma.$runCommandRaw({
+		dropDatabase: 1,
+	});
+
+	const user = await prisma.user.create({
+		data: {
+			name: "JP Calvo",
+			email: "calvojp92@gmail.com",
+			username: "calvojp",
+			password: await bcrypt.hash("password", await bcrypt.genSalt(16)),
+		},
+	});
+
+	const today = new Date();
+
+	await prisma.expense.createMany({
+		data: Array.from({length: 100}).map(() => ({
+			userId: user.id,
+			amount: faker.number.float({
+				min: 100,
+				max: 100000,
+				precision: 0.1,
+			}),
+			category: faker.helpers.arrayElement(Object.values(ExpenseCategory)),
+			location: faker.location.city(),
+			description: faker.word.sample(),
+			transactionDate: faker.date.between({
+				to: today,
+				from: subYears(today, 2),
+			}),
+		})),
+	});
+}
+
+main()
+	.catch(console.error)
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
