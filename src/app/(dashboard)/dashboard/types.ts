176c7854af8bc2e4bbe0fa55924b807671ffdd49
jp calvo <calvojp92@gmail.com>
2023-12-07 19:27:52 +0800

    fix types

--- src/app/(dashboard)/dashboard/types.ts
+++ src/app/(dashboard)/dashboard/types.ts
@@ -1,17 +1,22 @@
+import {ExpenseCategory} from "@prisma/client";
+
 export interface MonthData {
 	month: string;
 	index: number;
 	total: number;
+	category: ExpenseCategory;
 }
 
 export interface WeekData {
 	week: string;
 	index: number;
 	total: number;
+	category: ExpenseCategory;
 }
 
 export interface DayData {
 	day: string;
 	index: string;
 	total: string;
+	category: ExpenseCategory;
 }
